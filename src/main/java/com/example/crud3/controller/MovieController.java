2ce748c5e62cdec2fa8fbd22f123e25c21b317aa
Abraham <chobanu.ilya@icloud.com>
2022-02-19 02:19:58 +0200

    Added @ResponseStatus

--- src/main/java/com/example/crud3/controller/MovieController.java
+++ src/main/java/com/example/crud3/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.example.crud3.controller;
 
 import com.example.crud3.entity.Movie;
 import com.example.crud3.service.MovieService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,31 +18,37 @@ public class MovieController {
         this.movieService = movieService;
     }
     @PostMapping("/movies")
+    @ResponseStatus(HttpStatus.CREATED)
     public Movie saveMovie(@RequestBody Movie movie){
         return movieService.saveMovie(movie);
     }
 
     @GetMapping("/movies/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Movie findMovieById(@PathVariable Long id){
         return movieService.findMovieById(id);
     }
 
     @GetMapping("/movies")
+    @ResponseStatus(HttpStatus.OK)
     public List<Movie> findAllMovies(){
         return movieService.findAllMovies();
     }
 
     @PutMapping("/movies/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Movie updateMovie(@RequestBody Movie movie,@PathVariable Long id){
         return movieService.updateMovie(movie, id);
     }
 
     @DeleteMapping("/movies/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMovieById(@PathVariable Long id){
         movieService.deleteMovieById(id);
     }
 
     @DeleteMapping("/movies")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAllMovie(){
         movieService.deleteAllMovies();
     }
